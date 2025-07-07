231556e12ecf1a7b9c10539ea10997821117148e
Guilherme-07062002 <guilherme762002@gmail.com>
2025-07-07 19:45:44 -0300

    feat: atualizar controlador principal e configuração do aplicativo para incluir prefixo global e CORS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('auth');
-  app.enableCors();
+  app.setGlobalPrefix('auth', {
+    exclude: [{ path: '', method: RequestMethod.GET }],
+  });
+  app.enableCors({
+    origin: 'http://localhost:3000', // Permite requisições do frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Permite cookies e cabeçalhos de autenticação
+  });
   await app.listen(3000);
 }
 bootstrap();
